8409f155e4ac1f64fff663d628a0efe58ad92778
thewalterjr <89614640+thewalterjr@users.noreply.github.com>
2021-11-28 15:41:19 -0300

    Update Day1-DataType.js

--- 30-days-of-code/Day1-DataType.js
+++ 30-days-of-code/Day1-DataType.js
@@ -1,23 +1,3 @@
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
-
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
-
-process.stdin.on('data', function (data) {
-    input_stdin += data;
-});
-
-process.stdin.on('end', function () {
-    input_stdin_array = input_stdin.split("\n");
-    main();    
-});
-
-// Reads complete line from STDIN
-function readLine() {
-    return input_stdin_array[input_currentline++];
-}
 
 function main() {
     var i = 4
