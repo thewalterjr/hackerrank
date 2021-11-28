ed426c9f2a5ad8e63fe85da52757bf5826576937
thewalterjr <89614640+thewalterjr@users.noreply.github.com>
2021-11-28 15:40:30 -0300

    Update Day3-Conditional.js

--- 30-days-of-code/Day3-Conditional.js
+++ 30-days-of-code/Day3-Conditional.js
@@ -1,27 +1,3 @@
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-
-
 function main() {
     const N = parseInt(readLine().trim(), 10);
     
@@ -32,6 +8,5 @@ function main() {
     }
     else {
         console.log("Not Weird");
-    }
-    
+    } 
 }
