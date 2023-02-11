e7df88fecbd68788a89a306c0e71c17d4f034e39
thewalterjr <89614640+thewalterjr@users.noreply.github.com>
2023-02-11 12:05:36 -0300

    Update runningSum.ts

--- runningSum.ts
+++ runningSum.ts
@@ -11,3 +11,15 @@ function runningSum(nums: number[]): number[] {
     
     return newArr;
 };
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] runningSum = new int[nums.length];
+        runningSum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            runningSum[i] = runningSum[i-1] + nums[i];
+        }
+        return runningSum;
+    }
+}
+
